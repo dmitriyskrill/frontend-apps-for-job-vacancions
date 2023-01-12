03b3ebc42c4c2f9d3624a20e6bc63161b0d0da0a
Asus Vivobook <dmitriyskrill@gmail.com>
2023-01-12 23:47:00 +0300

    add Elevator entity, create class, first test is done

---
+++ src/entities/elevator/index.ts
@@ -0,0 +1,124 @@
+
+enum elevatorStatuses {  // Статусы лифта
+  RELAX = 'relax',
+  FREE = 'free',
+  MOVING = 'moving'
+}
+
+enum moveDirections {  // Статусы лифта
+  UP = 'up',
+  DOWN = 'down',
+}
+
+interface IElevatorInitProps {
+  relaxTime?: number | undefined,
+  travelTimePerFloor?: number | undefined
+  floor?: number | undefined
+  status?: string | undefined
+}
+
+class Elevator {
+
+  currentFloor: number
+  currentStatus: string
+  moveTimePerFloor: number
+  relaxTime: number
+  moveDirection: string | null = null
+  goalFloor: number | null = null
+  callStack: number[] = []
+
+  constructor (props: IElevatorInitProps) {
+    const { travelTimePerFloor, relaxTime, status, floor } = props
+    this.moveTimePerFloor = travelTimePerFloor || 1000
+    this.relaxTime = relaxTime || 3000
+    this.currentStatus = status || elevatorStatuses.FREE
+    this.currentFloor = floor || 1
+  }
+
+  get difference () {
+    if (this.goalFloor === null) return 0
+    return this.goalFloor - this.currentFloor
+  }
+
+  addFloorToCallStack (floor: number) {
+    if (floor === this.currentFloor && this.currentStatus === elevatorStatuses.FREE) return
+    const index = this.callStack.indexOf(floor)
+    if (index >= 0) return
+    this.callStack.push(floor)
+    if (this.currentStatus === elevatorStatuses.FREE) {
+      this.setGoalFloor(this.callStack[0])
+    }
+  }
+
+  setGoalFloor (floor: number) {
+    console.log('setGoalFloor', floor)
+    this.goalFloor = floor
+    this.moveToGoalFloor().finally()
+  }
+
+  async moveToGoalFloor () {
+    this.startMoving()
+    while (this.difference !== 0) {
+      await this.move()
+    }
+    await this.endMoving()
+  }
+
+  startMoving () {
+    console.log('startMoving')
+    if (this.callStack.length === 0) return
+    this.currentStatus = elevatorStatuses.MOVING
+  }
+
+  async move () {
+    console.log('move', this.difference, this.currentFloor, this.goalFloor, this.moveDirection, this.currentStatus)
+    if (this.difference > 0) {
+      await this.moveUp()
+    } else {
+      await this.moveDown()
+    }
+
+  }
+
+  async moveUp () {
+    this.moveDirection = moveDirections.UP
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        this.currentFloor++
+        resolve('')
+      }, this.moveTimePerFloor)
+    })
+  }
+
+  async moveDown () {
+    this.moveDirection = moveDirections.DOWN
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        this.currentFloor--
+        resolve('')
+      }, this.moveTimePerFloor)
+    })
+  }
+
+  async endRelaxing(){
+    return await new Promise((resolve) => {
+      setTimeout(() => {
+        if (this.callStack.length > 0) {
+          this.setGoalFloor(this.callStack[0])
+        } else {
+          this.currentStatus = elevatorStatuses.FREE
+        }
+        resolve('')
+      }, this.relaxTime)
+    })
+  }
+  async endMoving () {
+    this.currentStatus = elevatorStatuses.RELAX
+    this.moveDirection = null
+    this.callStack.splice(0, 1)
+    await this.endRelaxing()
+  }
+
+}
+
+export default Elevator
--- src/views/elevator/Emulator.vue
+++ src/views/elevator/Emulator.vue
@@ -1,16 +1,32 @@
-<script>
+<template>
+  {{ elevator }}
+  <!--  <v-btn @click="start">start</v-btn>-->
+  <v-btn @click="restartElevator">restartElevator</v-btn>
+  <v-btn @click="addFloorToCallStack">addFloorToCallStack</v-btn>
+  <v-text-field v-model.number="goalFloor" type="number" label="goalFloor"></v-text-field>
+</template>
+
+<script lang="js">
+import Elevator from '@/entities/elevator'
+
 export default {
   data: () => ({
-    props: ['elevator'],
-    elevatorLinks: [
-      ['Описание задания', 'mdi-clipboard-text-outline', '/elevator', true],
-      ['Настройки', 'mdi-cog-outline', '/elevator/settings', true],
-      ['Эмуляция', 'mdi-elevator', '/elevator/emulator', true],
-    ],
+    elevator: new Elevator({}),
+    goalFloor: 1,
   }),
+  methods: {
+    restartElevator () {
+      this.elevator = new Elevator({
+        travelTimePerFloor: 1000,
+        floor:5
+      })
+    },
+    addFloorToCallStack () {
+      this.elevator.addFloorToCallStack(this.goalFloor)
+    },
+    start () {
+      this.elevator.setGoalFloor(this.goalFloor)
+    },
+  },
 }
 </script>
-
-<template>
-
-</template>
